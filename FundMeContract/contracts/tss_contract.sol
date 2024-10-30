83b9d7621f3f6eb9da231fa307e3b292fbc822f2
H4rly <96372727+Magic1483@users.noreply.github.com>
2024-10-30 03:16:06 -0800

    add tss

---
+++ FundMeContract/contracts/tss_contract.sol
@@ -0,0 +1,68 @@
+
+/*
+   * @title Tramp Super Star
+   * @dev Tramp Super Star
+*/
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TrampSuperStar is ERC20, Ownable {
+    address contract_deployer;
+    address[] users;
+
+
+    modifier onlyDeployer() {
+        require(msg.sender == contract_deployer,"Only contract deployer can do this shit!!");
+        _;
+    }
+
+
+    constructor()  ERC20("Tramp Super Star","TSS") Ownable(msg.sender) { 
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1 million tokens to the deployer
+        contract_deployer = msg.sender;
+    }
+
+
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function isUserExist(address _addr) public view returns (bool)  {
+        for (uint i; i < users.length; i++) 
+        {
+            if (users[i] == _addr){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function burnAll() public onlyDeployer  {
+        for (uint i ; i < users.length; i++ ) 
+        {
+            _burn(users[i], balanceOf(users[i]));
+        }
+    }
+
+    function mintSomething(uint256 amount) public onlyDeployer {
+        _mint(contract_deployer, amount);
+    }
+
+    // block transfer for all except contract deployer
+    function transfer(address to, uint256 value) public override  returns (bool) {
+        if (!isUserExist(msg.sender)){
+            users.push(msg.sender);
+        }
+        if (!isUserExist(to)){
+            users.push(to);
+        }
+        return  super.transfer(to, value);
+    }
+
+    
+    
+}
--- FundMeContract/hardhat.config.js
+++ FundMeContract/hardhat.config.js
@@ -2,19 +2,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
 
-const {API_URL, PRIVATE_KEY} = process.env
+const {API_URL, PRIVATE_KEY,LH_PRIVATE_KEY} = process.env
 
 module.exports = {
-  solidity: "0.8.0",
-  defaultNetwork: "sepolia",
+  solidity: "0.8.20",
+  defaultNetwork: "localhost",
   networks: {
     sepolia: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
-    }
-    // localhost: {
-    //   url: "http://127.0.0.1:8545",
-    //   accounts: [process.env.prive_key]
-    // },
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: [`0x${LH_PRIVATE_KEY}`]
+    },
   }
 };
